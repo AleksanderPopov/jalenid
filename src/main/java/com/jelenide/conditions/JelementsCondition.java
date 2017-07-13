2a671427b6a731ae329533ca485386e65a15d512
Alexander Popov <aleksander.popov.93@gmail.com>
2017-07-14 00:29:41 +0300

    imptove semantic of conditions

--- src/main/java/com/jelenide/conditions/JelementsCondition.java
+++ src/main/java/com/jelenide/conditions/JelementsCondition.java
@@ -1,6 +1,7 @@
 package com.jelenide.conditions;
 
 
+import com.jelenide.Jelement;
 import com.jelenide.Jelements;
 
 import java.util.function.Function;
@@ -8,5 +9,5 @@ import java.util.function.Function;
 /**
  * Created by Alex on 7/9/2017.
  */
-public interface JelementsCondition extends Function<Jelements, Jelements> {
+public interface JelementsCondition<T extends Jelement> extends Function<Jelements<T>, Jelements<T>> {
 }
--- src/main/java/com/jelenide/conditions/JelementsConditions.java
+++ src/main/java/com/jelenide/conditions/JelementsConditions.java
@@ -1,5 +1,6 @@
 package com.jelenide.conditions;
 
+import com.jelenide.Jelement;
 import com.jelenide.Jelements;
 
 /**
@@ -7,28 +8,38 @@ import com.jelenide.Jelements;
  */
 public class JelementsConditions {
 
-  public static JelementsCondition size(int size) {
-    return new JelementsCondition() {
+  public static <T extends Jelement> JelementsCondition<T> size(int size) {
+    return new JelementsCondition<T>() {
       @Override
-      public Jelements apply(Jelements jelements) {
+      public Jelements<T> apply(Jelements<T> jelements) {
         try {
           return jelements.size() == size ? jelements : null;
         } catch (Exception e) {
           return null;
         }
       }
+
+      @Override
+      public String toString() {
+        return "$classname{}";
+      }
     };
   }
 
-  public static JelementsCondition sizeGreaterThan(int size) {
-    return new JelementsCondition() {
+  public static <T extends Jelement> JelementsCondition<T> sizeGreaterThan(int size) {
+    return new JelementsCondition<T>() {
       @Override
-      public Jelements apply(Jelements jelements) {
+      public Jelements<T> apply(Jelements<T> jelements) {
         try {
           return jelements.size() >= size ? jelements : null;
         } catch (Exception e) {
           return null;
         }
+      }
+
+      @Override
+      public String toString() {
+        return "$classname{}";
       }
     };
   }
